b5056875c30d2183b61071dd251a4e44b6dd8e12
iwe556 <davidbobiw3@gmail.com>
2026-05-21 23:13:25 +0100

    Some more progress

--- Level_01_Basics/03_reverse_number/solution.cpp
+++ Level_01_Basics/03_reverse_number/solution.cpp
@@ -1,9 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int reverseNumber(int number) {
+     int newInt = 0;
+
+    while (number != 0) {
+        int lastDigit = number % 10;
+
+        newInt = newInt * 10 + lastDigit;
+
+        number = number / 10;
+    }
+
+    return newInt;
+}
+
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
+
+    int input;
+
+    cout << "Input an number(integer): " << endl;
+    cin >> input;
+
+    cout << "Reversed number: " << reverseNumber(input);
 
     return 0;
 }
--- Level_01_Basics/04_strong_password_checker/solution.cpp
+++ Level_01_Basics/04_strong_password_checker/solution.cpp
@@ -1,9 +1,66 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+pair<bool, string> strongPassowrdChecker(string password) {
+    string message;
+    bool strongPassword;
+
+    unordered_map<string,bool> checkList = {
+        {"CapitalLetter",false},
+        {"SpecialCharacter",false},
+        {"AtleastEightCharacters",false},
+        {"NumberIncluded",false}
+    };
+
+    if (password.length() > 8) {
+        checkList.at("AtleastEightCharacters") = true;
+    }
+
+    for (int i = 0; i < password.length(); i++) {
+        if (!isalnum(password[i])) {
+            checkList.at("SpecialCharacter") = true;
+        }
+
+        if (isdigit(password[i])) {
+            checkList.at("NumberIncluded") = true;
+        }
+
+        if (isupper(password[i])) {
+            checkList.at("CapitalLetter") = true;
+        }
+    }
+
+    for (auto[requirement,status] : checkList) {
+        if (status == false) {
+            strongPassword = false;
+            message = message + requirement + " is false \n";
+        } else {
+            strongPassword = true;
+            message = message = message + requirement + " is true \n";
+        }
+    }
+
+    if (strongPassword) {
+        message = message + "Password is strong!";
+    } else {
+        message = message + "Password is not strong...";
+    }
+    
+    return {strongPassword,message};
+}
+
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
+
+    string password;
+
+    cout << "Input a password: " << endl;
+    cin >> password;
+
+    auto[result, message] = strongPassowrdChecker(password);
+
+    cout << message << endl;
 
     return 0;
 }
