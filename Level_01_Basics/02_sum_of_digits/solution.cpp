055f6b98853fd6a7e51055821b0a05d1c93914e9
iwe556 <davidbobiw3@gmail.com>
2026-05-21 10:05:30 +0100

    Completed the sum_of_digits problem

--- Level_01_Basics/02_sum_of_digits/solution.cpp
+++ Level_01_Basics/02_sum_of_digits/solution.cpp
@@ -1,9 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int sumDigitsOfInteger(int integer) {
+    int sum = 0;
+
+    while (integer != 0) {
+        int lastDigit = integer % 10; /* Get the last digit of the integer*/
+        sum = sum + lastDigit; /*Add the last digit to the sum*/
+        integer = integer / 10; /*Remove the last digit*/
+    }
+
+    return sum;
+}
+
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
+
+    int input;
+
+    cout << "Input number: " << endl;
+    cin >> input;
+
+    cout << "The sum of digits in " << input << " is " << sumDigitsOfInteger(input) << endl;
 
     return 0;
 }
