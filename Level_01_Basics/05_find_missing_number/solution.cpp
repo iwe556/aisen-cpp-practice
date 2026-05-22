d350799e2c8360e5563ac6f534204e827a380128
iwe556 <davidbobiw3@gmail.com>
2026-05-22 09:33:02 +0100

    Finished Level_01_Basics problems

--- Level_01_Basics/05_find_missing_number/solution.cpp
+++ Level_01_Basics/05_find_missing_number/solution.cpp
@@ -5,5 +5,21 @@ int main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
+    int n;
+    cin >> n;
+
+    int expectedSum = n * (n + 1) / 2;
+    int actualSum = 0;
+
+    for (int i = 0; i < n - 1; i++) {
+        int number;
+        cin >> number;
+        actualSum += number;
+    }
+
+    int missingNumber = expectedSum - actualSum;
+
+    cout << missingNumber << "\n";
+
     return 0;
 }
