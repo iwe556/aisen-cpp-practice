29880830ea09c8d4599403415cb9d644a1370be5
iwe556 <davidbobiw3@gmail.com>
2026-05-26 15:40:11 +0100

    Started Level_03_Strings

--- Level_03_Strings/11_clean_palindrome/solution.cpp
+++ Level_03_Strings/11_clean_palindrome/solution.cpp
@@ -5,5 +5,15 @@ int main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
+    string sentence;
+    getline(cin, sentence);
+
+    sentence.erase(remove(sentence.begin(), sentence.end(), ' '), sentence.end());
+    transform(sentence.begin(), sentence.end(), sentence.begin(), [](unsigned char c){ return std::tolower(c); });
+    
+    string reversedSentence(sentence.rbegin(),sentence.rend());
+
+    if (sentence == reversedSentence) cout << "Is a palindrome!"; else cout << "Is not a palindrome...";
+
     return 0;
 }
