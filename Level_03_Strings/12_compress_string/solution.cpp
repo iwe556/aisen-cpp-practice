f9170537b006a7db952887dd875d86515ab658a2
iwe556 <davidbobiw3@gmail.com>
2026-05-28 19:47:14 +0100

    Made some progress on Level_03_Strings

--- Level_03_Strings/12_compress_string/solution.cpp
+++ Level_03_Strings/12_compress_string/solution.cpp
@@ -5,5 +5,24 @@ int main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
+    string text;
+    cin >> text;
+
+    char currentCharacter;
+    int count = 1;
+
+    for (int i = 1; i < text.length(); i++) {
+        currentCharacter = text[i];
+
+        if (currentCharacter == text[i - 1]) {
+            count = count + 1;
+        } else {
+            cout << text[i - 1] << count;
+            count = 1;
+        }
+    }
+
+    cout << text[text.length() - 1] << to_string(count);
+
     return 0;
 }
--- Level_03_Strings/13_first_repeating_character/solution.cpp
+++ Level_03_Strings/13_first_repeating_character/solution.cpp
@@ -5,5 +5,25 @@ int main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
+    string text;
+    cin >> text;
+
+    vector<char> seen;
+
+    for (int i = 0; i < (int)text.length(); i++) {
+        char currChar = text[i];
+        auto iterator = find(seen.begin(), seen.end(), currChar);
+
+        if (iterator != seen.end()){
+            cout << currChar << '\n';
+            return 0;
+        } else {
+            seen.push_back(currChar);
+        }
+    }
+
+    cout << -1 << '\n';
+    
+
     return 0;
 }
