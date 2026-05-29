d350b59fbfd4a2d8695871f7182ec12b8e8c6da9
iwe556 <davidbobiw3@gmail.com>
2026-05-29 21:06:18 +0100

    Finished Level_03_Strings

--- Level_03_Strings/14_most_frequent_character/solution.cpp
+++ Level_03_Strings/14_most_frequent_character/solution.cpp
@@ -4,6 +4,33 @@ using namespace std;
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
+    
+    string text;
+    cin >> text;
+
+    unordered_map<char,int> letterCount = {
+        
+    };
+
+    for (int i = 0; i < text.length(); i++) {
+        auto it = letterCount.find(text[i]);
+
+        if (it != letterCount.end()) {
+            letterCount[text[i]] = letterCount[text[i]] + 1;
+        } else {
+            letterCount.insert({text[i],1});
+        }
+    }
+
+    auto maxIt = letterCount.begin();
+
+    for (auto it = letterCount.begin(); it != letterCount.end(); it++) {
+        if (it->second > maxIt->second) {
+            maxIt = it;
+        }
+    }
+
+    cout << "Most frequent character is: " << maxIt->first << " it occurs: " << maxIt->second << " time(s)";
 
     return 0;
 }
--- Level_03_Strings/15_count_words_in_sentence/solution.cpp
+++ Level_03_Strings/15_count_words_in_sentence/solution.cpp
@@ -5,5 +5,19 @@ int main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
+    string sentence;
+    getline(cin, sentence);
+
+    stringstream ss(sentence);
+
+    string word;
+    int count = 0;
+
+    while (ss >> word) {
+        count++;
+    }
+
+    cout << count << "\n";
+
     return 0;
 }
